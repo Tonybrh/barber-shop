bf124f67da1c94187956fe2a75039a2ff5ff3d24
Antônio Dias <antonio.eduardo@voxtec.com.br>
2025-03-23 22:33:43 -0300

    feat: created the email verification feature

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controller\Reservation\CreateReservationPostAction;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('reservation')->middleware('auth:sanctum')->group(function () {
+Route::prefix('reservation')->middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::post('/create', CreateReservationPostAction::class)->name('reservation.create');
 });
--- routes/auth.php
+++ routes/auth.php
@@ -2,7 +2,25 @@
 
 use App\Http\Controller\Auth\CreateUserPostAction;
 use App\Http\Controller\Auth\LoginPostAction;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', LoginPostAction::class)->name('login');
 Route::post('/register', CreateUserPostAction::class)->name('register');
+
+Route::get('/email/verify', function() {
+    return view('auth.verify-email');
+})->middleware('auth:sanctum')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
